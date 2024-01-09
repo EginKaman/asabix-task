68f0d52f032c589aa6f2f6001b52a52b5049825e
Ivan Tiutiunnyk <egin.kaman@gmail.com>
2024-01-09 11:53:40 +0200

    feat: quality tools was installed and running

--- config/app.php
+++ config/app.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -156,13 +157,9 @@ return [
     */
 
     'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
+        // Package Service Providers...
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        // Application Service Providers...
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
@@ -184,5 +181,4 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
-
 ];
